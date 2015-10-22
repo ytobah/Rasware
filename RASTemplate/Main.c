4c59a96b058e166b9961c59bebd95c21cbb64fb0
ytobah <ytobah@utexas.edu>
2015-10-22 15:44:49 -0500

    Testing Sparkfun RF Modules

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,24 +1,55 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
+#include <RASLib/inc/spi.h>
 
 // Blink the LED to show we're on
 tBoolean blink_on = true;
+tSPI SpiMod;
+tSPI * SpiPt;
+uint32_t * SendPt;
+uint32_t * RecPt;
+uint32_t SendLength;
+uint32_t RecLength;
+float WaitTime;
+
+tSPI rSpiMod;
+tSPI * rSpiPt;
+uint32_t * rSendPt;
+uint32_t * rRecPt;
+uint32_t rSendLength;
+uint32_t rRecLength;
 
 void blink(void) {
     SetPin(PIN_F3, blink_on);
     blink_on = !blink_on;
 }
 
+int ch=0;
 
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
     CallEvery(blink, 0, 0.5);
-    
+    SpiPt =  InitializeSPI(PIN_A2, PIN_A5, PIN_A4, 100, 16, true, 
+                            false);
+    Printf("Hello");
+    rSpiPt =  InitializeSPI(PIN_F2, PIN_F1, PIN_F0, 100, 16, true, 
+                            false);
+
     while (1) {
         // Runtime code can go here
-        Printf("Hello World!\n");
-        
+    while (ch =0)
+    ch = Getc
+    *SendPt = ch;
     }
+    SPIRequest(SpiPt, PIN_A3, SendPt, SendLength, RecPt, RecLength, 
+WaitTime);
+
+    SPIRequest(rSpiPt, PIN_A6, rSendPt, rSendLength, rRecPt, rRecLength, 
+WaitTime);
+
+    Printf("%s",  rRecPt);
+
+ch = 0;
 }
