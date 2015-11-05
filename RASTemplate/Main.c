9eee4dafcaa2bd5e8dbd4daa671a6ea80822ca3b
Joseph <josephvoss14@gmail.com>
2015-11-05 14:59:15 -0600

    Adds comments and various fixes

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -16,7 +16,7 @@ uint32_t * SendPt = &TestData;
 uint32_t * RecPt = &TestData2;
 uint32_t SendLength = 1;
 uint32_t RecLength = 1;
-float WaitTime = 0.5;
+float WaitTime = 1;
 tSPI * rSpiPt; //= &rspitest;
 uint32_t * rSendPt = &rTestD;
 uint32_t * rRecPt = &rTestD2;
@@ -35,13 +35,13 @@ int main(void) {
     // Initialization code can go here
     InitializeGPIO();
     CallEvery(blink, 0, 0.5);
-    SpiPt =  InitializeSPI(PIN_A2, PIN_A5, PIN_A4, 100, 16, false, 
-false);
-    Printf("Hello\n");
-    rSpiPt =  InitializeSPI(PIN_F2, PIN_F1, PIN_F0, 100, 16, false, 
+    SpiPt =  InitializeSPI(PIN_A2, PIN_A5, PIN_A4, 100, 8, false, 
+false); //Clock, mosi, miso, frequency, bitsPerFrame(?), clockPolarity(?), phase (?)
+    Printf("Hello\nIs it me you're looking for?");
+    rSpiPt =  InitializeSPI(PIN_F2, PIN_F1, PIN_F0, 100, 8, false, 
 false);
 
-SetPin(PIN_A7, true);
+SetPin(PIN_A7, true); //Manually set CE pins to receive/transmit
 SetPin(PIN_B2, false);
 
 
@@ -50,24 +50,26 @@ SetPin(PIN_B2, false);
 
         // Runtime code can go here
    while (ch == 0){
-    ch = Getc();
-   *SendPt = ch;
+     ch = Getc();
+// 	 ch = 0x57;
+     *SendPt = ch;
    Printf("Working\n");
 }
    Printf("%c\n", ch);
 
-  Check = SPIRequestUS(SpiPt, PIN_A3, SendPt, SendLength, RecPt, 
-RecLength, WaitTime);
+   Check = SPIRequestUS(SpiPt, PIN_A3, SendPt, SendLength, RecPt, 
+0, WaitTime);
+   WaitUS(100);
    Printf("%c Again!!\n",ch);
    if (Check){
    Printf("True\n");
     }
-   SPIRequestUS(rSpiPt, PIN_B7, rSendPt, rSendLength, rRecPt, 
-   rRecLength, WaitTime);
+   SPIRequestUS(rSpiPt, PIN_B7, rSendPt, 0, rRecPt, 
+   rRecLength, 100);
 
-    Printf("%c", ch);
-    ch = *rRecPt;
-    Printf("%c\n", ch);
+   Printf("%c", ch);
+   ch = *rRecPt;
+   Printf("%c\n", ch);
 //Wait(2);
 ch = 0;
 }
