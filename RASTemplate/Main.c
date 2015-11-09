7dedf5f03fc4dc51f56f49cf3f41d4feb5e71e5b
ytobah <ytobah@utexas.edu>
2015-11-09 15:06:15 -0600

    Attempted to Use Commands (But behaves the same)

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -14,8 +14,8 @@ uint32_t rTestD;
 uint32_t rTestD2;
 uint32_t * SendPt = &TestData;
 uint32_t * RecPt = &TestData2;
-uint32_t SendLength = 1;
-uint32_t RecLength = 1;
+uint32_t SendLength = 4;
+uint32_t RecLength = 4;
 float WaitTime = 1;
 tSPI * rSpiPt; //= &rspitest;
 uint32_t * rSendPt = &rTestD;
@@ -35,14 +35,12 @@ int main(void) {
     // Initialization code can go here
     InitializeGPIO();
     CallEvery(blink, 0, 0.5);
-    SpiPt =  InitializeSPI(PIN_A2, PIN_A5, PIN_A4, 100, 8, false, 
-false); //Clock, mosi, miso, frequency, bitsPerFrame(?), clockPolarity(?), phase (?)
+    SpiPt =  InitializeSPI(PIN_A2, PIN_A5, PIN_A4, 100, 8, false, false); //Clock, mosi, miso, frequency, bitsPerFrame(?), clockPolarity(?), phase (?)
     Printf("Hello\nIs it me you're looking for?");
-    rSpiPt =  InitializeSPI(PIN_F2, PIN_F1, PIN_F0, 100, 8, false, 
-false);
+    rSpiPt =  InitializeSPI(PIN_F2, PIN_F1, PIN_F0, 100, 8, false, false);
 
 SetPin(PIN_A7, true); //Manually set CE pins to receive/transmit
-SetPin(PIN_B2, false);
+SetPin(PIN_B2, false );
 
 
     while (1) {
@@ -50,25 +48,34 @@ SetPin(PIN_B2, false);
 
         // Runtime code can go here
    while (ch == 0){
-     ch = Getc();
-// 	 ch = 0x57;
-     *SendPt = ch;
+  //   ch = Getc();
+   ch = 0x64;
+    // *(SendPt + 3) = ch;
    Printf("Working\n");
 }
    Printf("%c\n", ch);
-
+   *SendPt = 0x64A08220;  //Little Endian: Write Command, Config for Send, 
+                          //Send Command, then Data to be sent
+  // *(SendPt++)=0x82;
    Check = SPIRequestUS(SpiPt, PIN_A3, SendPt, SendLength, RecPt, 
-0, WaitTime);
-   WaitUS(100);
+  RecLength, WaitTime);
+  SetPin(PIN_B2, true);
+   WaitUS(10);
+  SetPin(PIN_B2, false);
+
    Printf("%c Again!!\n",ch);
    if (Check){
    Printf("True\n");
     }
-   SPIRequestUS(rSpiPt, PIN_B7, rSendPt, 0, rRecPt, 
-   rRecLength, 100);
 
+   SetPin(PIN_A7, false);  //Disable Receiver to Read Data
+   *SendPt = 0x55618320;        //Write Command, Config for Receive  
+                                 // ReceiveCommand, and Dummy Variable
+   SPIRequestUS(rSpiPt, PIN_B7, SendPt, 4, RecPt, 1, 
+WaitTime);
+   SetPin(PIN_A7, true);   //Enable Receiver for next batch
    Printf("%c", ch);
-   ch = *rRecPt;
+   ch = *RecPt;
    Printf("%c\n", ch);
 //Wait(2);
 ch = 0;
